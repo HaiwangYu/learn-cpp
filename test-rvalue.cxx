d7e8595022bbf1ade1c9ae2978836ee7a266edfe
HaiwangYu <yuhw.pku@gmail.com>
2024-01-06 12:21:44 -0500

    Update processString_l to process lvalues correctly

--- test-rvalue.cxx
+++ test-rvalue.cxx
@@ -10,10 +10,10 @@ void processString_r(std::string&& s) {
 }
 
 void processString_l(std::string& s) {
-    std::cout << "Processing rvalue: " << s << std::endl;
+    std::cout << "Processing lvalue: " << s << std::endl;
     // Modify the string to demonstrate that we have an actual reference
     s += " (modified)";
-    std::cout << "Modified rvalue: " << s << std::endl;
+    std::cout << "Modified lvalue: " << s << std::endl;
 }
 
 std::string generateHello() {
@@ -22,7 +22,11 @@ std::string generateHello() {
 
 int main() {
     processString_r(generateHello()); // OK
+
     // processString_l(generateHello()); // WRONG: initial value of reference to non-const must be an lvalueC/C++(461)
+    std::string s = generateHello();
+    processString_l(s); // OK
+    std::cout << "Modified lvalue: " << s << std::endl;
 
     return 0;
 }
